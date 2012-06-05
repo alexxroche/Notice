812aa27aa7b367a9fa3f24637c1daa8e11b4596a
Alexx Roche <alexx@cpan.org>
2012-06-05 11:13:23 +0200

    more HTML5 ish and the start of the security admin

---
+++ lib/Notice/C/Security.pm
@@ -0,0 +1,111 @@
+package Notice::C::Security;
+
+use warnings;
+use strict;
+use base 'Notice';
+my %submenu = (
+   '1.0' => [
+        '2' => { peer => 1, name=> 'Security', rm => 'security', class=> 'navigation'},
+    ],
+);
+
+
+=head1 NAME
+
+Template controller subclass for Notice
+
+=head1 ABSTRACT
+
+Template for consistent controller creation.
+
+=head1 DESCRIPTION
+
+Take a wild guess from the name
+
+=head1 METHODS
+
+=head2 SUBCLASSED METHODS
+
+=head3 setup
+
+Override or add to configuration supplied by Notice::cgiapp_init.
+
+=cut
+
+sub setup {
+    my ($self) = @_;
+    $self->authen->protected_runmodes(':all');
+    $self->tt_params({submenu => \%submenu});
+}
+
+=head2 RUN MODES
+
+=head3 main
+
+This displays data about the security of Notice, how often certain things are accessed
+and by whom
+
+=cut
+
+sub main: StartRunmode {
+    my ($self) = @_;
+    my ($username,$message,%sec_stats,@people);
+       $username = $self->authen->username;
+    if($username && $username ne 'a@b.com'){
+        $self->tt_params({ message => 'I will not warn you again! You are not meant to be in here.' });
+        $self->plt;
+        return $self->tt_process();
+    }
+    my $pe_id; # person that we are sooping on
+
+    our $user_details; # everyone
+
+    if($pe_id && $pe_id=~m/^\d+$/){
+        @people = $self->resultset('People')->search();
+    }else{
+        @people = $self->resultset('People')->search();
+    }
+
+    $self->tt_params({s => \%sec_stats, message => $message, ppl => \@people});
+    $self->plt;
+    return $self->tt_process();
+}
+
+1;
+
+__END__
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known problems with this module.
+Please fix any bugs or add any features you need. 
+You can report them through GitHub or CPAN.
+
+=head1 SEE ALSO
+
+L<Notice>, L<CGI::Application>
+
+=head1 SUPPORT AND DOCUMENTATION
+
+You could look for information at:
+
+    Notice@GitHub
+        http://github.com/alexxroche/Notice
+
+=head1 AUTHOR
+
+Alexx Roche, C<alexx@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2011 Alexx Roche
+
+This program is free software; you can redistribute it and/or modify it
+under the following license: Eclipse Public License, Version 1.0
+or the Artistic License.
+
+See http://www.opensource.org/licenses/ for more information.
+
+=cut
+
+
