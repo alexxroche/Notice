118646e9a93c6c5972f2f7acf8ae8469975cfc9e
Alexx Roche <alexx@cpan.org>
2012-12-05 15:42:00 +0100

    More example modules and an addendum
    Ironically to the Addendum module itself.
    The example modules are, at this time, mostly sketches.

--- script/de-dupe_dump.sh
+++ script/de-dupe_dump.sh
@@ -6,10 +6,12 @@
 # 46 7,*/2 * * * pushd /var/www/sites/Notice 2>&1 1>/dev/null; ./script/dump_notice_db.pl; popd 2>&1 1>/dev/null
 
 FMATCH='notice_*.mysql'
+THIS_DIR='config'
 
-for FSIZE in `ls -la $FMATCH|awk '{print $5}'|sort -u`
-do  
-    ls -l|grep $FSIZE|tail -n$(( $(ls -la|grep $FSIZE|wc -l) - 1))|awk '{print $NF}'|xargs rm -f
+for FSIZE in `ls -la $THIS_DIR/$FMATCH|awk '{print $5}'|sort -u`
+do
+    #echo $FSIZE
+    ls -l $THIS_DIR/$FMATCH|grep $FSIZE|tail -n$(( $(ls -la $THIS_DIR/$FMATCH|grep $FSIZE|wc -l) - 1))|awk '{print $NF}'|xargs rm -f
 done
 
 # if my awk was stronger I'd probably start with something like
