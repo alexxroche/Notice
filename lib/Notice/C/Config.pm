07e746789ea1db78754df5cb90a3d7b2319da0d7
Alexx Roche <alexx@cpan.org>
2013-02-05 04:26:12 +0100

    Per-account CSS editing for those in the HR group
    We are using codemirror as an example

--- lib/Notice/C/Config.pm
+++ lib/Notice/C/Config.pm
@@ -10,6 +10,9 @@ my %submenu = (
    '1.0' => [
         '1' => { peer => 1, name=> 'Modules', rm => 'modules', class=> 'navigation'},
         '2' => { peer => 1, name=> 'Security', rm => 'security', class=> 'navigation'},
+        '3' => { peer => 0, name=> 'Greeting', rm => 'greet', class=> 'navigation'},
+        '4' => { peer => 0, name=> 'CSS', rm => 'css', class=> 'navigation'},
+        '5' => { peer => 0, name=> 'Logo', rm => 'logo', class=> 'navigation'},
     ],
 );
 
@@ -20,7 +23,7 @@ Notice::C::Config - Template controller subclass for Notice
 
 =head1 ABSTRACT
 
-This is where Notice is configured from. Only those in the Notice_Admin group
+This is where Notice is 0onfigured from. Only those in the Notice_Admin group
 should have access here. The user equivelent is "Your Details"
 
 =head1 DESCRIPTION
@@ -41,6 +44,7 @@ Override or add to configuration supplied by Notice::cgiapp_init.
 sub setup {
     my ($self) = @_;
     $self->authen->protected_runmodes(':all');
+    $self->tt_params({ submenu => \%submenu });
 }
 
 =head2 RUN MODES
@@ -89,10 +93,245 @@ sub main: StartRunmode {
     $self->tt_params({
     action  => "$surl/aliases",
     domains => \@domains,
-    submenu => \%submenu,
 	message => $message,
     body    => $body
 		  });
+    return $self->tt_process();
+}
+
+
+=head3 css
+
+    set the CSS for this account
+
+=cut
+
+sub css: Runmode {
+    my ($self) = @_;
+    my ($css,$message,$error,$page);
+    my $q = $self->query;
+    my $surl;
+       $surl = ($self->query->self_url);
+    my ($ac_id);
+    if($self->param('ef_acid')){ $ac_id = $self->param('ef_acid'); }
+    elsif($self->param('ac_id')){ $ac_id = $self->param('ac_id'); }
+
+    if( $self->in_group('HR',$self->param('pe_id')) || $self->param('pe_id') == 1){
+
+        $self->tt_params({ is_admin => 1 });
+
+        my $acc_css = '';
+        if(defined $self->cfg("www_path")){
+            my $css_path = $self->cfg("www_path") . "/css";
+            if(-f "$css_path/${ac_id}.css"){ 
+                $acc_css = "${ac_id}.css"; 
+                open(CSS, "<$css_path/${ac_id}.css" );
+                while(my $line = <CSS>){
+                    $css .= $line;
+                }
+                close(CSS);
+            }else{ $css = "/* css for Account: $ac_id */"; }
+            $self->tt_params({ name => "${ac_id}.css" });
+        }
+
+        my $code;
+        my $old_css_name;
+        my $css_name;
+        if( $q->param('update') && (
+                    $q->param('update') eq "Add" ||
+                    $q->param('update') eq "Save" ||
+                    $q->param('update') eq "Update"
+                    )
+        ){
+            #use DateTime qw /now/;
+            #my $now = DateTime->now();         # these three lines work but why bother?
+            $code = $q->param('code');
+            $old_css_name = $q->param('id');
+            $css_name = $q->param('name');
+
+            if($css && $code ne $css){
+                # NTS you are here writing this back to 
+                if(length($css) < length($code)){
+                    $message = "Increaed CSS by " . ( length($code) - length($css) ) . ' characters';
+                }else{
+                    $message = "CSS reduced by " . ( length($css) - length($code) ) . ' bytes';
+                }
+                if(defined $self->cfg("www_path")){
+                    my $css_path = $self->cfg("www_path") . "/css";
+                    unless(-d "$css_path"){
+                        `mkdir -p "$css_path"`;
+                    }
+                    if(-w "$css_path/${ac_id}.css"){
+                        $acc_css = "${ac_id}.css";
+                        open(CSS, ">$css_path/${ac_id}.css" ) or $message = 'Could not write CSS to file';
+                        print CSS $code;
+                        close(CSS);
+                        $css = $code;
+                    }else{
+                        $self->tt_params({ error => 'CSS path does not exist'});
+                    }
+                }
+
+            }else{
+                warn "no css or code eq css";
+            }
+
+        }
+
+        if($acc_css){ 
+            $page .= "this account has a default layout called " . $acc_css;
+        }else{
+            $page .= "this account FALLS back to the default css of main.css";
+            $acc_css = 'main.css';
+        }
+            
+        #if($self->param('css')){
+        #    $page .= " but THIS user has a personal override of " . $self->param('css');
+        #}else{
+        #    $page .= ' - you can edit the account\'s CSS here';
+        #}
+
+        # we don't check per-page CSS
+
+        $self->tt_params({
+        action  => "$surl/css",
+        message => $message,
+        msg => $page,
+        editor1 => $css,
+              });
+        #return $self->tt_process('default.tmpl');
+        return $self->tt_process();
+    }else{
+       return $self->tt_process('default.tmpl', {error => 'You have to be in the HR group to edit the account CSS' });
+       warn "You " . $self->param('pe_id') . " are not in the HR group";
+    }
+}
+
+
+=head3 logo
+   
+    set the logo at the top
+   
+=cut
+   
+sub logo: Runmode {
+    my ($self) = @_;
+    my ($welcome,$message);
+    my $q = $self->query;
+    my $surl;
+       $surl = ($self->query->self_url);
+    my ($ac_id);
+    if($self->param('ef_acid')){ $ac_id = $self->param('ef_acid'); }
+    elsif($self->param('ac_id')){ $ac_id = $self->param('ac_id'); }
+    $message = 'Here you will be able to upload a small Logo for this account';
+   
+    if( $self->in_group('HR',$self->param('pe_id')) || $self->param('pe_id') == 1){
+        $self->tt_params({ is_admin => 1 });
+   
+        $self->tt_params({
+        action  => "$surl/aliases",
+        page => $message,
+              });
+        return $self->tt_process('default.tmpl');
+    }
+}
+
+
+=head3 greet
+
+  * The message that users get on the front page AFTER logging in
+  - if they do not have a default homepage
+
+=cut
+
+sub greet: Runmode {
+    my ($self) = @_;
+    my ($welcome,$message);
+    my $q = $self->query;
+    my $surl;
+       $surl = ($self->query->self_url);
+    my ($ac_id);
+    if($self->param('ef_acid')){ $ac_id = $self->param('ef_acid'); }
+    elsif($self->param('ac_id')){ $ac_id = $self->param('ac_id'); }
+
+    if( $self->in_group('HR',$self->param('pe_id')) || $self->param('pe_id') == 1){ 
+        $self->tt_params({ is_admin => 1 }); 
+
+        my $cf_id = 12; # this should be pulled from Conf!
+
+        if($q->param('update') || $q->param('add') ){
+            my $wel_rc = $self->resultset('ConfData')->search({
+                                      -and => [
+                                                 cfd_key => 'welcome',
+                                                 cfd_acid => "$ac_id"
+                                                ]})->first;
+            my $msg_rc = $self->resultset('ConfData')->search({
+                                      -and => [
+                                                 cfd_key => 'message',
+                                                 cfd_acid => "$ac_id"
+                                                ]})->first;
+            #if($q->param('update')){
+            #    warn "we are doing an " . $q->param('update') . ' for acc: ' . $ac_id;
+            #}elsif( $q->param('add') ){
+            #    warn "we are doing an " . $q->param('add') . ' for account: ' . $ac_id;
+            #    warn " Welcome: " . $q->param('welcome') . ' with a message of length ' .  length($q->param('message'));
+            #}
+            # NOTE we should only update if it has changed
+            if($wel_rc){
+                $wel_rc->update({ cfd_value => $q->param('welcome') })->update;
+                $message .= " Welcome updated ";
+            }else{
+                $self->resultset('ConfData')->create({ cfd_cfid => $cf_id, cfd_acid => $ac_id, cfd_key => 'welcome', cfd_value => $q->param('welcome') })->update;
+                $message .= " Welcome created ";
+            }
+            if($msg_rc){
+                $msg_rc->update({ cfd_value => $q->param('message') })->update;
+                $message .= " message updated ";
+            }else{
+                $self->resultset('ConfData')->create({ cfd_cfid => $cf_id, cfd_acid => $ac_id, cfd_key => 'message', cfd_value => $q->param('message') })->update;
+                $message .= " message created ";
+            }
+            $self->tt_params({ message => $message });
+        }
+    }
+
+    if($ac_id){
+        # NTS need to join the group table so that we only list domains that are not in the
+        # "no email" domains group
+        my $conf_rc = $self->resultset('ConfData')->search({
+            -and => [
+             cf_name => 'greeting',
+             cfd_acid => "$ac_id"
+            ]
+             },{
+            join => ['config']
+        });
+       my $ac = $self->resultset('Account')->search({ ac_id => "$ac_id" })->first;
+
+       if($ac && ( $ac->ac_name || $ac->ac_id )){
+        if($ac->ac_name){
+           $self->tt_params({ ac_name => $ac->ac_name });
+        }
+        $self->tt_params({ ac_id => $ac->ac_id });
+       } 
+        
+       if($conf_rc){
+
+            while(my $cfd = $conf_rc->next){
+                if($cfd->cfd_key eq 'welcome'){
+                    $welcome = $cfd->cfd_value;
+                }elsif($cfd->cfd_key eq 'message'){
+                    $message = $cfd->cfd_value;
+                }
+            }
+
+            $self->tt_params({
+                main_welcome => $welcome,
+                main_message => $message
+               });
+       }
+       # if we have an update then we should do that here
+    }
     return $self->tt_process();
 }
 
@@ -109,7 +348,7 @@ sub ajax_alter_div1 : Runmode {
     # foreach my $sp ( $self->param ){ warn "$sp = " . $self->param($sp); } #debug
     return $self->redirect($self->query->url . '/' . $self->param('mod'));
   }else{
-    scalar localtime . $self->query->p('Ajax example, (Look Ma, no page reload!)');
+    scalar localtime() . $self->query->p('Ajax example, (Look Ma, no page reload!)');
   }
     
 }
